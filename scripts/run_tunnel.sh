e74aba4309f352a48ad79b7511f6eff056f4eaee
Preethi chowdary Bollipalli <preethibollipalli@gmail.com>
2025-08-11 13:19:28 -0500

    Update run_tunnel.sh

--- scripts/run_tunnel.sh
+++ scripts/run_tunnel.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+# usage: ./scripts/run_tunnel.sh <ec2-public-ip> </path/to/key.pem>
+set -euo pipefail
+EC2_IP="${1:?provide EC2 public IP}"
+KEY="${2:?provide path to pem key}"
+ssh -i "$KEY" -N -L 8080:localhost:8080 "ubuntu@${EC2_IP}"
 
